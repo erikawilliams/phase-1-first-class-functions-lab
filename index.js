88ab794091e9edaf88e3ce732c34ef4eebf5f5db
Erika Williams <erika@erikawilliams.us>
2022-03-15 10:45:52 -0600

    commit tree f64bb9b3071fc6a5af90c6545480b4c927a4f119

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (inputArray) {
+    return inputArray.slice(0,2)
+}
+
+const returnLastTwoDrivers = function (inputArray) {
+    return inputArray.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers]
+
+const createFareMultiplier = function (multiplier) {
+    let fareMultiplier = fare => fare*multiplier
+    return fareMultiplier
+}
+
+const fareDoubler = function(fare) {
+    return createFareMultiplier(2)(fare)
+}
+
+const fareTripler = function(fare) {
+    return createFareMultiplier(3)(fare)
+}
+
+const selectDifferentDrivers = function(drivers,f) {
+    return f(drivers);
+}
